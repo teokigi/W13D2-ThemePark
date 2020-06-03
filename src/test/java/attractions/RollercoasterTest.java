2d83bae1bcfb2815a45b60bcb2ced4ee470785f3
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-03 08:09:51 +0100

    rollercoastertest
    added 3 tests for testing ISecurity, where visitor has to be over 145 height and 12 age

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -4,19 +4,23 @@ import org.junit.Before;
 import org.junit.Test;
 import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
     Visitor visitor;
     Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
         visitor = new Visitor(21,250,100);
         visitor2 = new Visitor(21,190,100);
+        visitor3 = new Visitor(11,190,100);
+        visitor4 = new Visitor(13,120,100);
     }
 
     @Test
@@ -42,4 +46,10 @@ public class RollercoasterTest {
         assertEquals(4.50,rollerCoaster.priceFor(visitor2),0.01);
     }
 
+    @Test public void canRide(){assertTrue(rollerCoaster.isAllowedTo(visitor));}
+
+    @Test public void cannotRide(){
+        assertFalse(rollerCoaster.isAllowedTo(visitor3));
+        assertFalse(rollerCoaster.isAllowedTo(visitor4));
+    }
 }
