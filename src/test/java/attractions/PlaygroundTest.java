06a6db09e8a2310ca2c1699c65718afe1d753f4f
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-03 07:58:42 +0100

    Playground
    age check at playground, tested and passed

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor = new Visitor(15,150,100);
+        visitor2 = new Visitor(17,200,100);
     }
 
     @Test
@@ -27,4 +32,10 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void passSecurity(){ assertTrue(playground.isAllowedTo(visitor));}
+
+    @Test
+    public void failsSecurity(){ assertFalse(playground.isAllowedTo(visitor2));}
 }
